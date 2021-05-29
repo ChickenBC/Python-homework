3c3b3b52f5bba9b80d859d83d01ea3576f27fad5
ChickenBC <59474826+ChickenBC@users.noreply.github.com>
2021-05-29 23:33:12 +0800

    Update 9-8.py

--- Chapter9/9-8.py
+++ Chapter9/9-8.py
@@ -1,48 +1,46 @@
 class User():
-    
-    def __init__(self, first_name, last_name, username, email, location):
-        #初始化用户
-        self.first_name = first_name.title()
-        self.last_name = last_name.title()
-        self.username = username
-        self.email = email
-        self.location = location.title()
 
-    def describe_user(self):
-        print("\n" + self.first_name + " " + self.last_name)
-        print("  Username: " + self.username)
-        print("  Email: " + self.email)
-        print("  Location: " + self.location)
+	def __init__(self, first_name, last_name, username, email, location):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.username = username
+		self.email = email
+		self.location = location
 
-    def greet_user(self):
-        print("\nWelcome back, " + self.username + "!")
+	def describe_user(self):
+		print("\n" + self.first_name + " " + self.last_name)
+		print("Username: " + self.username)
+		print("Email: " + self.email)
+		print("Location: " + self.location)
 
-class Admin(User):
-    def __init__(self, first_name, last_name, username, email, location):
-        super().__init__(first_name, last_name, username, email, location)
-        self.privileges = []
-    
-    def show_privileges(self):
-        for privilege in self.privileges:
-            print("- " + privilege)
+	def greet_user(self):
+		print("\nWelcome back, " + self.username + "!")
+
 
 class Privileges():
 
-    def __init__(self, privileges=[]):
-        self.privileges = privileges
+	def __init__(self, privileges=[]):
+		#注意列表的表示方法
+		self.privileges = privileges
 
-    def show_privileges(self):
-        print("\nPrivileges:")
-        if self.privileges:
-            for privilege in self.privileges:
-                print("- " + privilege)
-        else:
-            print("- This user has no privileges.")
+	def show_privileges(self):
+		print("\nPrivileges:")
+		if self.privileges:
+			for privilege in self.privileges:
+				print("- " + privilege)
+		else:
+			print("- This user has no privileges.")
+
+class Admin(User):
+
+	def __init__(self, first_name, last_name, username, email, location):
+		super().__init__(first_name, last_name, username, email, location)
+		self.privilege = Privileges()
 
 eric = Admin('eric', 'matthes', 'e_matthes', 'e_matthes@example.com', 'alaska')
 eric.describe_user()
 
-eric.privileges.show_privileges()
+eric.privilege.show_privileges()
 
 print("\nAdding privileges...")
 eric_privileges = [
@@ -50,5 +48,5 @@ eric_privileges = [
     'can moderate discussions',
     'can suspend accounts',
     ]
-eric.privileges.privileges = eric_privileges
-eric.privileges.show_privileges()
+eric.privilege.privileges = eric_privileges
+eric.privilege.show_privileges()
