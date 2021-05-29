8cac46e02d54d6720b05f443147363238478d0d9
ChickenBC <59474826+ChickenBC@users.noreply.github.com>
2021-05-29 23:43:31 +0800

    Update 9-9.py

--- Chapter9/9-9.py
+++ Chapter9/9-9.py
@@ -55,9 +55,14 @@ class ElectricCar(Car):
         super().__init__(manufacturer, model, year)
         self.battery = Battery()
 
-my_tesla = ElectricCar('tesla', 'model s', '2016')
-print(my_tesla.get_descriptive_name())
+print("Make an electric car, and check the battery:")
+my_tesla = ElectricCar('tesla', 'model s', 2016)
 my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
+
+print("\nUpgrade the battery, and check it again:")
 my_tesla.battery.upgrade_battery()
-my_tesla.battery.get_range()
+my_tesla.battery.describe_battery()
+
+print("\nTry upgrading the battery a second time.")
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.describe_battery()
