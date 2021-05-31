fc77043224591fb917178fbf989069a893a5205e
ChickenBC <59474826+ChickenBC@users.noreply.github.com>
2021-05-31 22:54:13 +0800

    Update pi_string.py

--- Chapter10/pi_string.py
+++ Chapter10/pi_string.py
@@ -2,10 +2,12 @@ filename = 'pi_digits.txt'
 
 with open(filename) as file_object:
   lines = file_object.readlines()
+  #readline()只读取第一行
 
 pi_string = ' '
 for line in lines:
   pi_string += line.strip()
+  #strip()用于删除空格
   
 print(pi_string)
 print(len(pi_string))
